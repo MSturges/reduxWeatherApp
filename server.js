b8efd1586b2849b48d0cfd90fbb2e6c674781e5c
MSturges <Sturges3232@gmail.com>
2016-10-24 10:15:53 -0600

    updated www file to reference the correct project name and removed es6 from server side code and replaced with es5

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const path = require('path');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-// const api = require('./routes/api');
+// var api = require('./routes/api');
 
-const app = express();
+var app = express();
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -15,18 +15,18 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // app.use('/', api);
 
-app.all('*', (req,res,next) => {
+app.all('*', function(req,res,next) {
   res.sendFile('index.html', { root: __dirname + '/dist/' })
 })
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 if (app.get('env') === 'development') {
-  app.use((err, req, res, next) => {
+  app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
