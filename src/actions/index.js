a51053e9eb9ab975d514f2306451f14b27e88488
MSturges <Sturges3232@gmail.com>
2016-10-24 10:25:25 -0600

    wather get now https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = '6e1b409c4efadce6fc257a6bc208f330';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
